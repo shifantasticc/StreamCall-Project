49c468572b12b33d64cbb0118dc4b758667c3a0d
shifantasticc <shifastudyit@gmail.com>
2025-10-21 04:12:33 +0530

    Adding App.js File

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
 import './App.css';
-import { AuthProvider } from './contexts/AuthContext';
-import Authentication from './pages/authentication';
-import LandingPage from './pages/landing';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LandingPage from './pages/landing';
+import Authentication from './pages/authentication';
+import { AuthProvider } from './contexts/AuthContext';
 import VideoMeetComponent from './pages/VideoMeet';
 import HomeComponent from './pages/home';
+import History from './pages/history';
 
 function App() {
   return (
-    <>
+    <div className="App">
+
       <Router>
+
         <AuthProvider>
+
+
           <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/auth" element={<Authentication />} />
-            <Route path="/home" element={<HomeComponent />} />
-            <Route path="/:url" element={<VideoMeetComponent />} />
+
+            <Route path='/' element={<LandingPage />} />
+
+            <Route path='/auth' element={<Authentication />} />
+
+            <Route path='/home's element={<HomeComponent />} />
+            <Route path='/history' element={<History />} />
+            <Route path='/:url' element={<VideoMeetComponent />} />
           </Routes>
         </AuthProvider>
+
       </Router>
-    </>
+    </div>
   );
 }
 
