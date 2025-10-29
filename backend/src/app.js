d1bcf3468fabe0d967e16540a8e1a90c2faa65e4
shifantasticc <shifastudyit@gmail.com>
2025-10-29 16:24:22 +0530

    Adding app.js File

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,37 +1,39 @@
-import express, { urlencoded } from 'express';
-import { createServer } from 'node:http';
+import express from "express";
+import { createServer } from "node:http";
 
-import { Server } from 'socket.io';
+import { Server } from "socket.io";
 
-import mongoose from 'mongoose';
-import { connectToSocket } from './controllers/socketManager.js';
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
 
-import cors from 'cors';
-import userRoutes from './routes/users.routes.js';
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
 
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
-app.set('port', process.env.PORT || 8000);
-app.use(cors());
-app.use(express.json({ limit: '40kb' }));
-app.use(express.urlencoded({ limit: '40kb', extended: true }));
 
-app.use('/api/v1/users', userRoutes);
-// make Version routes for "if version changes so for it to handle"
-// app.use('/api/v2/users', newUserRoutes); 
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
-  app.set('mongo_user');
-  const connectionDb = await mongoose.connect(
-    'mongodb+srv://shifastudyit:M2P5zNKbJ1NLQfFU@clustercall.wv07bzc.mongodb.net/',
-  );
+    app.set("mongo_user")
+    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
 
-  console.log(`MONGO connected DB Host: ${connectionDb.connection.host}`);
-  server.listen(app.get('port'), () => {
-    console.log('Listing at port 8000');
-  });
-};
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENIN ON PORT 8000")
+    });
+
+
+
+}
+
+
 
 start();
